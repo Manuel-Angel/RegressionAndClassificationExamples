d889bf1db6a803a0f3c99ea8bd781ca7139fdde9
Manuel-Angel <manu.ang6587@gmail.com>
2017-03-01 21:04:52 -0600

    PolynomialRegression.buildClassifier and classyfyInstance was implemented, App.main has tests methods for this method.

--- src/main/java/org/manuel/examples/RegressionAndClassificationExamples/App.java
+++ src/main/java/org/manuel/examples/RegressionAndClassificationExamples/App.java
@@ -1,13 +1,54 @@
 package org.manuel.examples.RegressionAndClassificationExamples;
 
+import java.util.ArrayList;
+
+import org.manuel.examples.RegressionAndClassificationExamples.weka.PolynomialRegression;
+
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+    	PolynomialRegression classifier= new PolynomialRegression(2);
+    	double datos[][]=new double[15][2];
+    	for (int i = 0; i < datos.length; i++) {
+			datos[i][0]= i;
+			datos[i][1]= i*i;
+		}
+    	Instances ins= instancesFromArrays(datos);
+    	classifier.buildClassifier(ins);
+    	
+    	double x[]=new double[]{20, 400};
+    	double y=classifier.classifyInstance(new DenseInstance(1.0, x));
+    	
+        System.out.println("y= "+y);
     }
+    public static Instances instancesFromArrays(double [][]values) {
+		ArrayList<Attribute> attribs = new ArrayList<>(values[0].length);
+		for (int i = 0; i < values[0].length-1; i++) {
+			attribs.add(new Attribute("x" + i));
+		}
+		attribs.add(new Attribute("y"));
+		
+		Instances inst= new Instances("lineal", attribs, values.length);
+		inst.setClassIndex(values[0].length-1);
+		
+		for (int i = 0; i < values.length; i++) {
+			inst.add(instance(values[i],inst));
+		}
+		return inst;
+	}
+    public static Instance instance(double values[], Instances data){
+		Instance inst = new DenseInstance(1.0, values);
+		inst.setDataset(data);//this is optional, just for using methods about the attributes
+		return inst;
+	}
 }
--- src/main/java/org/manuel/examples/RegressionAndClassificationExamples/weka/PolynomialRegression.java
+++ src/main/java/org/manuel/examples/RegressionAndClassificationExamples/weka/PolynomialRegression.java
@@ -3,9 +3,11 @@ package org.manuel.examples.RegressionAndClassificationExamples.weka;
 import java.util.ArrayList;
 
 import weka.classifiers.Classifier;
+import weka.classifiers.bayes.net.search.fixed.FromFile;
 import weka.classifiers.meta.FilteredClassifier;
 import weka.core.Attribute;
 import weka.core.Capabilities;
+import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.UnassignedClassException;
@@ -13,12 +15,13 @@ import weka.filters.Filter;
 
 public class PolynomialRegression implements Classifier {
 	weka.classifiers.functions.LinearRegression classifier;
+	Instances instances;
 	int degree; 
 	/**
 	 * Creates a classifier for performing polynomial regression of the degree specified 
 	 * @param degree the degree of the polynomial model to build
 	 */
-	PolynomialRegression(int degree){
+	public PolynomialRegression(int degree){
 		classifier= new weka.classifiers.functions.LinearRegression();
 		this.degree=degree;
 	}
@@ -28,35 +31,97 @@ public class PolynomialRegression implements Classifier {
 		if(data.classIndex()<0){
 			throw new UnassignedClassException("Class index is negative (not set)!");
 		}
-		ArrayList<Attribute> attribs = new ArrayList<>(data.numAttributes() * degree);
-		
-		Instances newInstances = new Instances(data.relationName()+" ", attribs, data.numInstances());
-		for (int i = 0; i < data.numInstances(); i++) {
-			data.get(i);
+		ArrayList<Attribute> attribs = new ArrayList<>((data.numAttributes() -1) * degree);
+		int att;
+		for (att = 0; att < data.numAttributes(); att++) {
+			attribs.add(data.attribute(att));
 		}
+		
+		for (int j = 0; j < data.numAttributes(); j++) {
+			if(data.classIndex()==j)
+				continue;
+			
+			for (int d = 2; d <= degree; d++) {
+				StringBuilder sb= new StringBuilder(data.attribute(j).name());
+				sb.append("_").append(d);
+				
+				attribs.add(new Attribute(sb.toString()));
+			}
+		}
+		
+		Instances newInstances = new Instances(data.relationName()+"polynomial", attribs, data.numInstances());
+		for (int in = 0; in < data.numInstances(); in++) {
+			Instance inst= data.get(in);
+			/*double values[]=new double[newInstances.numAttributes()];
+			
+			for (att = 0; att < inst.numAttributes(); att++) {
+				values[att]=inst.value(att);
+			}
+			
+			for(int j=0;j<inst.numAttributes();j++){
+				if(inst.classIndex() == j){
+					continue;
+				}
+				double aux= values[j];
+				for (int d= 2; d <= degree; d++) {
+					aux*=values[j];
+					values[att++]= aux;
+				}
+			}
+			
+			Instance newInst = new DenseInstance(1.0, values);*/
+			Instance newInst = changeInstance(inst, newInstances.numAttributes());
+			newInstances.add(newInst);
+		}
+		instances= newInstances;
+		instances.setClassIndex(data.classIndex());
+		classifier.buildClassifier(newInstances);
+		
 		/*FilteredClassifier f = new FilteredClassifier(); //se puede hacer usando otras clases
 		Filter filter=;
 		f.setClassifier(classifier);
 		f.setFilter(filter);
 		*/
 	}
-
+	protected Instance changeInstance(Instance inst, int numAtt){
+		double values[]=new double[numAtt];
+		int att;
+		for (att = 0; att < inst.numAttributes(); att++) {
+			values[att]=inst.value(att);
+		}
+		for(int j=0;j<inst.numAttributes();j++){
+			if(inst.classIndex() == j){
+				continue;
+			}
+			double aux= values[j];
+			for (int d= 2; d <= degree; d++) {
+				aux*=values[j];
+				values[att++]= aux;
+			}
+		}
+		Instance newInst = new DenseInstance(1.0, values);
+		return newInst;
+	}
 	@Override
 	public double classifyInstance(Instance instance) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		instance.setDataset(instances);
+		Instance newInst= changeInstance(instance,instances.numAttributes());
+		double coeff[]= classifier.coefficients();
+		
+		
+		return classifier.classifyInstance(newInst);
 	}
 
 	@Override
 	public double[] distributionForInstance(Instance instance) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return classifier.distributionForInstance(instance);
 	}
 
 	@Override
 	public Capabilities getCapabilities() {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO Auto-generated method stub
+		return classifier.getCapabilities();
 	}
 	
 }
